ae4dc6ef319906ecd6410dc46d11093885e1c52f
Adrian Deambroggi <adrian.m.deambroggi@gmail.com>
2023-10-31 10:53:31 -0300

    add funcion de verificacion de email

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,55 @@
 const bcryptjs = require('bcryptjs')
 const Users = require('../models/userModel')
 const crypto = require('crypto')
+const nodemailer = require('nodemailer')
+const {google} = require('googleapis') 
+const OAuth2 = google.auth.OAuth2
+
+const sendMail = async (email, uniqueString)=>{
+
+    const myOAuth2Client = new OAuth2(
+        process.env.GOOGLE_CLIENTID,
+        process.env.GOOGLE_SECRET,
+        "https://developers.google.com/oauthplayground"
+    )
+
+    myOAuth2Client.setCredentials({refresh_token:process.env.GOOGLE_REFRESHTOKEN})
+
+    const accessToken = myOAuth2Client.getAccessToken()
+
+    const transporter = nodemailer.createTransport({
+
+        service: "gmail",
+        auth:{
+            user:"useremailverifymindhub@gmail.com",
+            type: "OAuth2",
+            clientId: process.env.GOOGLE_CLIENTID,
+            clientSecret:process.env.GOOGLE_SECRET,
+            refreshToken: process.env.GOOGLE_REFRESHTOKEN,
+            accessToken: accessToken
+        },
+        tls:{
+            rejectUnauthorized: false
+        } 
+    })
+
+    const mailOptions = {
+        from: "useremailverifymindhub@gmail.com",
+        to: email,
+        subject: "Validacion de mail",
+        html:`
+        <div>
+        <h1><a href=http://localhost:5000/api/users/auth/verifyEmail/${uniqueString}>CLICK </a> para validar tu cuenta</h1>
+        </div>
+        `
+    }
+
+    transporter.sendMail(mailOptions, function(error, response){
+        if(error){console.log(error)}
+        else{console.log("mensaje enviado")}
+    })
+
+}
 
 const userControllers = {
 
@@ -54,6 +103,7 @@ const userControllers = {
                 if (from === "signUp-form") {
 
                     await nuevoUsuario.save()
+                    sendMail(email, uniqueString)
                     //Funcion que envia el mail
                     res.json({
                         success: true,
